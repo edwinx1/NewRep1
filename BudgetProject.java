ff4faf81d39d2a19a61aa0341a7560090f85f169
edwinx1 <edwinxalv@gmail.com>
2024-09-04 03:01:43 +0000

    final commit for week 2

--- BudgetProject.java
+++ BudgetProject.java
@@ -10,12 +10,12 @@ public class BudgetProject
       final double rent_percent = 0.30;
       final double grocery_percent = 0.15;
       final double entertainment_percent = 0.10;
-        double savedamount;
-        double rentamount;
-        double groceriesamount;
-        double entertainmentamount;
-        double totalexpenses;
-        double remainingbalace;
+          double savedamount;
+          double rentamount;
+          double groceriesamount;
+          double entertainmentamount;
+          double totalexpenses;
+          double remainingbalace;
           savedamount = salary * savings_percent;
           rentamount = salary * rent_percent;
           groceriesamount = salary * grocery_percent;
